f35e90d0dfec102fef99ac46606aae20f684578e
aniketsuryavanshi093 <aniketsuryavanshi093@gmail.com>
2025-02-22 14:27:46 +0530

    images path changes

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -12,42 +12,37 @@ const getinitailstate = () => {
             id: 1,
             title: "Headphones sony max12h",
             price: 120,
-            image: "../../public/assets/images/c-d-x-PDX_a_82obo-unsplash.jpg",
+            image: "/assets/images/c-d-x-PDX_a_82obo-unsplash.jpg",
           },
           {
             id: 2,
             title: "Sony Max12h",
             price: 110,
-            image:
-              "../../public/assets/images/domino-studio-164_6wVEHfI-unsplash.jpg",
+            image: "/assets/images/domino-studio-164_6wVEHfI-unsplash.jpg",
           },
           {
             id: 3,
             title: "Sony Max12h",
             price: 110,
-            image:
-              "../../public/assets/images/giorgio-trovato-K62u25Jk6vo-unsplash.jpg",
+            image: "/assets/images/giorgio-trovato-K62u25Jk6vo-unsplash.jpg",
           },
           {
             id: 4,
             title: "Sony Max12h",
             price: 110,
-            image:
-              "../../public/assets/images/irene-kredenets-KStSiM1UvPw-unsplash.jpg",
+            image: "/assets/images/irene-kredenets-KStSiM1UvPw-unsplash.jpg",
           },
           {
             id: 5,
             title: "Sony Max12h",
             price: 110,
-            image:
-              "../../public/assets/images/olena-bohovyk-ElfJDs4LAQk-unsplash.jpg",
+            image: "/assets/images/olena-bohovyk-ElfJDs4LAQk-unsplash.jpg",
           },
           {
             id: 6,
             title: "Sony Max12h",
             price: 110,
-            image:
-              "../../public/assets/images/rachit-tank-2cFZ_FB08UM-unsplash.jpg",
+            image: "/assets/images/rachit-tank-2cFZ_FB08UM-unsplash.jpg",
           },
         ],
         cartList: [],
@@ -61,42 +56,37 @@ const getinitailstate = () => {
           id: 1,
           title: "Headphones sony max12h",
           price: 120,
-          image: "../../public/assets/images/c-d-x-PDX_a_82obo-unsplash.jpg",
+          image: "/assets/images/c-d-x-PDX_a_82obo-unsplash.jpg",
         },
         {
           id: 2,
           title: "Sony Max12h",
           price: 110,
-          image:
-            "../../public/assets/images/domino-studio-164_6wVEHfI-unsplash.jpg",
+          image: "/assets/images/domino-studio-164_6wVEHfI-unsplash.jpg",
         },
         {
           id: 3,
           title: "Sony Max12h",
           price: 110,
-          image:
-            "../../public/assets/images/giorgio-trovato-K62u25Jk6vo-unsplash.jpg",
+          image: "/assets/images/giorgio-trovato-K62u25Jk6vo-unsplash.jpg",
         },
         {
           id: 4,
           title: "Sony Max12h",
           price: 110,
-          image:
-            "../../public/assets/images/irene-kredenets-KStSiM1UvPw-unsplash.jpg",
+          image: "/assets/images/irene-kredenets-KStSiM1UvPw-unsplash.jpg",
         },
         {
           id: 5,
           title: "Sony Max12h",
           price: 110,
-          image:
-            "../../public/assets/images/olena-bohovyk-ElfJDs4LAQk-unsplash.jpg",
+          image: "/assets/images/olena-bohovyk-ElfJDs4LAQk-unsplash.jpg",
         },
         {
           id: 6,
           title: "Sony Max12h",
           price: 110,
-          image:
-            "../../public/assets/images/rachit-tank-2cFZ_FB08UM-unsplash.jpg",
+          image: "/assets/images/rachit-tank-2cFZ_FB08UM-unsplash.jpg",
         },
       ],
       cartList: [],
